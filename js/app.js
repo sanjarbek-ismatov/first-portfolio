b038db2e7e7f06799e8937effc6de0877f8e3fd9
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-01-06 10:32:37 +0500

    updated

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const hiddenLi = document.querySelector('.menu-hidden');
-const liClick = document.querySelector('.menu');
-liClick.addEventListener('click', function() {
-    if (hiddenLi.style.display == 'block') {
-        hiddenLi.style.display = 'none';
-    }
-    hiddenLi.style.display = 'block';
-});
